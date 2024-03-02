085e54f000bcaa265e14e20aec7d571f3338fb00
unknown <onwuesiwilliamsemeka@gmail.com>
2024-03-02 09:34:09 -0800

    pushing all project files to the new repo

---
+++ app/create_payment/page.jsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React, { useState } from "react"
+import { PaystackButton } from "react-paystack"
+
+
+const App = () => {
+  const publicKey = "pk_test_ea6fba45f769b68c74017a0896374653390e103c";
+  const amount = 1000000
+  const [email, setEmail] = useState("")
+  const [name, setName] = useState("")
+  const [phone, setPhone] = useState("")
+
+  const componentProps = {
+    email,
+    amount,
+    metadata: {
+      name,
+      phone,
+    },
+    publicKey,
+    text: "Pay Now",
+    onSuccess: () =>
+      alert("Thanks for doing business with us! Come back soon!!"),
+    onClose: () => alert("Wait! Don't leave :("),
+  }
+
+  return (
+    <div className="App">
+
+      <div className="container bg-yellow-500 p-10">
+
+        <div className="item">
+          <img alt="Item Image" src="./legitg.jpg" className="item-image"/>
+          <div className="item-details">
+            <p className="item-details__title">API Integration Services</p>
+            <p className="item-details__amount">${amount}</p>
+          </div>
+        </div>
+
+        <div className="checkout">
+
+          <form className="checkout-form">
+
+            <div className="checkout-field">
+              <label>Name</label>
+              <input
+                type="text"
+                id="name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+
+            <div className="checkout-field">
+              <label>Email</label>
+              <input
+                type="text"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="checkout-field">
+              <label>Phone</label>
+              <input
+                type="text"
+                id="phone"
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+
+          </form>
+
+          <PaystackButton {...componentProps} className="paystack-button"/>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ app/page.jsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Home() {
+
+  const [email, setEmail] = useState("");
+  const [amount, setAmount] = useState("");
+  const [error, setError] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    console.log("Email: ", email);
+    console.log("Message: ", amount);
+
+    const res = await fetch("api/contacts", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        amount,
+      }),
+    });
+
+    const { msg, success } = await res.json();
+
+    setError(msg);
+    setSuccess(success);
+
+    if (success) {
+      setEmail("");
+      setAmount("");
+    }
+  };
+
+  return (
+    <>
+
+  <div className="test">
+
+    <div className="relative min-h-screen w-full bg-[url('../public/legitg.jpg')] bg-cover bg-no-repeat">
+      <div className="absolute inset-0 h-full w-full bg-gray-900/60" />
+      <div className="grid min-h-screen px-8">
+        <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
+
+          <div>
+              <a href="/create_payment">
+                <button className="p-6 m-5 bg-green-600">
+                  Book API Integration Service
+                </button>
+              </a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+      <form
+        onSubmit={handleSubmit}
+        className="py-4 mt-4 border-t flex flex-col gap-5"
+      >
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="text"
+            className="h-10"
+            id="email"
+            placeholder="john@gmail.com"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="message">Amount</label>
+          <input
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+            type="text"
+            className="h-10"
+            id="amount"
+            placeholder="enter amount to pay here ..."
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" />
+        </div>
+
+        <button className="bg-green-700 p-3 text-white font-bold" type="submit">
+          Create Account
+        </button>
+
+      </form>
+
+
+      <div className="bg-slate-100 flex flex-col">
+        {error &&
+          error.map((e) => (
+            <div
+              className={`${
+                success ? "text-green-800" : "text-red-600"
+              } px-5 py-2`}
+            >
+              {e}
+            </div>
+          ))}
+      </div>
+
+    </>
+  );
+
+}
+ 
